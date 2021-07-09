bfc5be74619d67d23a9c91e70c8371a40d002921
ledia massoud <32582491+ledia8@users.noreply.github.com>
2021-07-09 05:14:49 +0200

    Add files via upload

--- website/app.js
+++ website/app.js
@@ -3,3 +3,18 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const apiKey = "bc588237d60453c472a120f03732c6bd";
+
+const generatButton = document.getElementById("generate");
+generatButton.addEventListener('click', async ()=>{
+    const zipCodeValue = document.getElementById("zip").value;//querySelector("#zip").vlue
+    if(!zipCodeValue){
+        alert("Enter Zip code!");
+    }
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCodeValue}&appid=${apiKey}&units=metric`);
+    const data = await res.json();
+    const temp = data.main.temp;
+    console.log(data);
+    console.log(temp);
+});
